ca8786580d7821dde0719c1655da5c800424b9c7
nachshon760 <n@boti.bot>
2026-07-08 15:05:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 3000,
   GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
-  SERVICE_ACCOUNT: require('./service-account.json'), // שים את הקובץ שנתת לי כאן
-  BOTI_BASE_URL: 'https://boti.bot',
-  API_KEY_LENGTH: 67
+  SERVICE_ACCOUNT: require('./service-account.json'),
+  BOTI_BASE_URL: 'https://api.360.boti.bot',   // <--- עדכון חשוב
+  CHANNEL_ID: "35148",      // מה-URL שלך
+  CHANNEL_HASH: "56e3ceb8"  // החלף בהאש המלא אם יש יותר תווים
 };
